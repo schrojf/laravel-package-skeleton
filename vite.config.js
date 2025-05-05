adbe4be2c639170452fc39c9a733f173d8e599c4
Viliam Schrojf <viliam@schrojf.sk>
2025-05-05 16:04:23 +0200

    Add support for older browser

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
+import legacy from "@vitejs/plugin-legacy";
+
 /** @type {import('vite').UserConfig} */
 export default {
+    plugins: [
+        // TODO: Implement this in app.blade.php
+        // legacy({
+        //     targets: ["defaults", "> 0.2% and not dead"],
+        // }),
+    ],
     build: {
         assetsDir: "",
         rollupOptions: {
